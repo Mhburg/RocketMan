df2d072e4af01eebbadcd6c6c4c0bd7b0970c5b7
Mhburg <Morgan_lee5@hotmail.com>
2020-07-19 22:13:17 -0400

    Add project files.

---
+++ RocketMan/src/HarmonyPatches/StatExtension_Patch.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace RocketMan
+{
+    [StaticConstructorOnStartup]
+    public static class StatExtension_Patch
+    {
+        private static Dictionary<StatRequestModel, CacheableTick<float>> _cache = new Dictionary<StatRequestModel, CacheableTick<float>>(StatRequestModelComparer.Instance);
+
+        private static MethodInfo _original = typeof(StatWorker).GetMethod(nameof(StatWorker.GetValue), new Type[] { typeof(StatRequest), typeof(bool) });
+
+        private static MethodInfo _prefix = typeof(StatExtension_Patch).GetMethod(nameof(StatExtension_Patch.Prefix), BindingFlags.Public | BindingFlags.Static);
+
+        private static MethodInfo _postfix = typeof(StatExtension_Patch).GetMethod(nameof(StatExtension_Patch.Postfix), BindingFlags.Public | BindingFlags.Static);
+
+        private static Stopwatch _stopwatch = new Stopwatch();
+
+        private static int _lastTick = -1;
+
+        private static int _calls = 0;
+
+        private static double _totalTime;
+
+        private static int _cacheHit = 0;
+
+        private static object _lock = new object();
+
+        private static List<string> _record = new List<string>();
+
+        static StatExtension_Patch()
+        {
+            HarmonyUtility.Instance.Patch(_original, new HarmonyMethod(_prefix), new HarmonyMethod(_postfix));
+        }
+
+        public static bool Prefix(StatRequest req, bool applyPostProcess, StatDef ___stat, ref float __result, out StatRequestModel __state)
+        {
+            //if (Prefs.DevMode && Current.Game != null)
+            //{
+            //    if (_lastTick != Find.TickManager.TicksGame)
+            //    {
+            //        //Log.Warning($"Execution time for tick {_lastTick} is {_totalTime}ms for {_calls} of calls. Total number of cache: {_cache.Count}. Cache hit: {_cacheHit}");
+            //        _totalTime = 0;
+            //        _calls = 0;
+            //        _cacheHit = 0;
+            //        _lastTick = Find.TickManager.TicksGame;
+
+            //        //Log.Message($"All requests for tick {_lastTick}\n{string.Join("\n", _record)}");
+            //        _record.Clear();
+            //    }
+
+            //    _stopwatch.Restart();
+            //}
+
+            __state = new StatRequestModel(req, applyPostProcess, ___stat);
+            if (_cache.TryGetValue(__state, out CacheableTick<float> value) && !value.ShouldUpdate(out _))
+            {
+                __result = value.Value;
+                //if (Prefs.DevMode)
+                //{
+                //    lock (_lock)
+                //    {
+                //        ++_cacheHit;
+                //    }
+                //}
+
+                //if (Prefs.DevMode && Current.Game != null)
+                //    _record.Add($"{__state.StatRequest} -- value: {value: 00.00}");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void Postfix(StatDef ___stat, ref float __result, StatRequestModel __state)
+        {
+            //if (Prefs.DevMode)
+            //{
+            //    if (Current.Game != null)
+            //    {
+            //        if (_stopwatch.IsRunning)
+            //        {
+            //            _stopwatch.Stop();
+            //            _totalTime += _stopwatch.Elapsed.TotalMilliseconds;
+            //            ++_calls;
+            //        }
+            //    }
+            //}
+
+            if (Current.Game != null)
+            {
+                if (_cache.TryGetValue(__state, out CacheableTick<float> value))
+                {
+                    value.Value = __result;
+                }
+                else
+                {
+                    _cache[__state] = MakeCache(__result);
+                }
+
+                //if (Prefs.DevMode && Current.Game != null)
+                //    _record.Add($"{__state.StatRequest} -- value: {__result: 00.00}");
+            }
+        }
+
+        private static CacheableTick<float> MakeCache(float initValue)
+        {
+            return new CacheableTick<float>(
+                initValue
+                , () => Find.TickManager.TicksGame
+                , 1
+                , null
+                , Find.TickManager.TicksGame);
+        }
+    }
+}
+
---
+++ RocketMan/src/Model/StatRequestModelComparer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace RocketMan
+{
+    public class StatRequestModelComparer : EqualityComparer<StatRequestModel>
+    {
+        public static StatRequestModelComparer Instance { get; } = new StatRequestModelComparer();
+
+        public override bool Equals(StatRequestModel x, StatRequestModel y)
+        {
+            if (x.Stat != y.Stat)
+                return false;
+
+            StatRequest xReq = x.StatRequest;
+            StatRequest yReq = y.StatRequest;
+
+            if (xReq.Thing == yReq.Thing
+                && xReq.StuffDef == yReq.StuffDef
+                && xReq.QualityCategory == yReq.QualityCategory
+                && xReq.Def == yReq.Def
+                && xReq.Faction == yReq.Faction
+                && xReq.Pawn == yReq.Pawn
+                && x.ApplyPostProcess == y.ApplyPostProcess)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode(StatRequestModel obj)
+        {
+            StatRequest statRequest = obj.StatRequest;
+
+            unchecked
+            {
+                int hash;
+                hash = HashOne(obj.Stat.shortHash);
+                hash = HashOne(statRequest.Thing?.thingIDNumber ?? 0, hash);
+                hash = HashOne(statRequest.StuffDef?.GetHashCode() ?? 0, hash);
+                hash = HashOne((int)statRequest.QualityCategory, hash);
+                hash = HashOne(statRequest.Def?.GetHashCode() ?? 0, hash);
+                hash = HashOne(statRequest.Faction?.loadID ?? 0, hash);
+                hash = HashOne(statRequest.Pawn?.thingIDNumber ?? 0, hash);
+                hash = HashOne(obj.ApplyPostProcess ? 1 : 0, hash);
+
+                return hash;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj as StatRequestModelComparer != null;
+        }
+
+        public override int GetHashCode()
+        {
+            return typeof(StatRequestModelComparer).Name.GetHashCode();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int HashOne(int numberToHash, int previousHash = 17)
+        {
+            return previousHash * 7919 + numberToHash;
+        }
+
+    }
+}
